88dd378c3e90bf960486dccf0081c2702d851e32
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-11 11:30:19 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+client.on('ready', () => {
+
+    console.log('I am ready!');
+    }
+});
+const prefix = "?";
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix)) return;
+
+ if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("Pong!");
+	   } else
+     	  if (message.content.startsWith(prefix + "am i manly")) {
+    message.channel.send("Yes, you are manly! :thumbsup:");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
