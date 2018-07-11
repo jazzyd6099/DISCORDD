7ce8cc51fe8053532908c44396b269ecdc366d46
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-11 11:33:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,14 +10,12 @@ client.on('ready', () => {
 });
 const prefix = "?";
 client.on("message", (message) => {
-  if (!message.content.startsWith(prefix)) return;
-
- if (message.content.startsWith(prefix + "ping")) {
+	  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("Pong!");
-	   } else
-     	  if (message.content.startsWith(prefix + "am i manly")) {
+  } else
+	  if (message.content.startsWith(prefix + "am i manly")) {
     message.channel.send("Yes, you are manly! :thumbsup:");
   }
 });
-
 client.login(process.env.BOT_TOKEN);
