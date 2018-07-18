4ac03e7a069dccffab5323aca2fa86ab80d0436e
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-17 22:51:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+const status = "invisible"
 
  
 client.on('ready', () => {
 
     console.log('I am ready!');
 	
-client.user.setStatus("Invisible")
+    client.user.setStatus(status)
 
 client.user.setActivity("Currently working out!")
 });
