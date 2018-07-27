ff9189038931270cef318ff3e3535a23ea7db229
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-26 23:25:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,11 +57,8 @@ if (message.content.startsWith("Tch.")) {
 	if (message.content.startsWith("I love my bot family")) {
 	message.channel.send("Aw, I love you too! :D");
 		} else
-		if (message.content.startsWith("i hate myself")) {
+		if (message.content.startsWith("i hate myself".toLowerCase() || message.content == "I hate myself" ) || message.content == "I hate myself.".toLowerCase()){
 			message.reply("Aw don't hate yourself. You're a manly person!");
-			} else
-		if (message.content.startsWith("i hate myself".toLowerCase() ) || message.content == "I hate myself" ) || message.content == "I hate myself.".toLowerCase()){
-			message.reply("Don't hate yourself! I love you!");
 			 } else 
 	 if (message.content == "Is there anybody online?".toLowerCase() || message.content == "Is there anybody online?" || message.content == "Is anybody online?".toLowerCase() || message.content == "Is anybody online".toLowerCase()){
      	   message.channel.send("I'm online! :D");
